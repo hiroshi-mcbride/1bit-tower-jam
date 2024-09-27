06fb4a1167da73ee184e252bba4caa9449e6ff6a
Hiroshi McBride <>
2024-09-27 19:21:23 +0200

    using rapier physics lib. somewhat improved arms

--- src/player/icepick.gd
+++ src/player/icepick.gd
@@ -1,54 +1,55 @@
 class_name IceAxe
 extends RigidBody2D
 
+@export var is_left_hand: bool
+@export var hand: PinJoint2D
+@export var hand_torque: float
+
+@export var shoulder: PinJoint2D
+@export var shoulder_torque: float
+
 @onready var timer: Timer = $Timer
 
-const SWING_VELOCITY = -50.0
-const OTHER_SWING_VELOCITY = 25.0
-var is_swinging: bool
-var joint: PinJoint2D
-var other_joint: PinJoint2D
+var is_swinging: bool = false
+var is_on_wall: bool = false
+
 
 
 func _ready() -> void:
+	#HACK: pin joint gets super weird at rotations around 180, so i'm rotating the child nodes instead
+	if is_left_hand:
+		$Sprite2D.rotation_degrees = 180
+		$Area2D.rotation_degrees = 180
+		$CollisionShape2D.rotation_degrees = 180
 	pass
 
 
-func _physics_process(delta: float) -> void:
-	if is_swinging:
-		pass
-
-
-func swing(joint: PinJoint2D, other_joint: PinJoint2D) -> void:
-	self.joint = joint
-	
+func swing() -> void:
 	is_swinging = true
-	joint.motor_target_velocity = SWING_VELOCITY
-	other_joint.motor_target_velocity = OTHER_SWING_VELOCITY
-	other_joint.motor_enabled = true
-	joint.motor_enabled = true
-	
-	timer.start()
+	shoulder.motor_target_velocity = -shoulder_torque
+	shoulder.motor_enabled = true
+	hand.motor_target_velocity = -hand_torque
+	hand.motor_enabled = true
 
 
-func drop(joint: PinJoint2D, other_joint: PinJoint2D) -> void:
-	self.joint = joint
-	
+func drop() -> void:
 	is_swinging = false
-	joint.motor_target_velocity = 0
-	other_joint.motor_target_velocity = 0
-	other_joint.motor_enabled = false
-	joint.motor_enabled = false
-	
-	freeze = false
+	shoulder.motor_target_velocity = 0
+	shoulder.motor_enabled = false
+	#hand.motor_target_velocity = 0
+	#hand.motor_enabled = false
+
+
+func pull() -> void:
+	shoulder.motor_target_velocity = -shoulder_torque * 0.5
+	shoulder.motor_enabled = true
+
+
+func hold() -> void:
+	is_swinging = false
+	shoulder.motor_target_velocity = 0
+	shoulder.motor_enabled = false
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if timer.is_stopped():
-		is_swinging = false
-	
-		if joint:
-			joint.motor_target_velocity = 0
-			joint.motor_enabled = false
-	
-		freeze = true
+	hold()
--- src/player/player.gd
+++ src/player/player.gd
@@ -4,18 +4,19 @@ extends Node2D
 @export var ice_axe_right: IceAxe
 
 @onready var left_pivot: PinJoint2D = $Shoulder_Q
-@onready var right_pivot: PinJoint2D = $Shoulder_E
+@onready var right_pivot: PinJoint2D = $Shoulder_Q
 
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("axe_left") && !ice_axe_right.is_swinging:
-		ice_axe_left.swing(left_pivot, right_pivot)
+	if Input.is_action_just_pressed("axe_left"):
+		ice_axe_left.swing()
+		#ice_axe_right.pull()
 	
 	if Input.is_action_just_released("axe_left"):
-		ice_axe_left.drop(left_pivot, right_pivot)
+		ice_axe_left.drop()
 	
-	if Input.is_action_just_pressed("axe_right") && !ice_axe_left.is_swinging:
-		ice_axe_right.swing(right_pivot, left_pivot)
+	if Input.is_action_just_pressed("axe_right"):
+		ice_axe_right.swing()
 	
 	if Input.is_action_just_released("axe_right"):
-		ice_axe_right.drop(right_pivot, left_pivot)
+		ice_axe_right.drop()
