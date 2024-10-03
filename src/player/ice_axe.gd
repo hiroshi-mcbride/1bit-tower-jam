bc8219e260f6fd0d232c26b1accd0856d81aac47
Hiroshi McBride <>
2024-10-03 16:17:24 +0200

    overhauled climbing

--- src/player/ice_axe.gd
+++ src/player/ice_axe.gd
@@ -10,43 +10,69 @@ extends RigidBody2D
 @export var shoulder_torque: float
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
-@onready var area_2d: Area2D = $Area2D
 @onready var trigger: CollisionShape2D = $Area2D/Trigger
 @onready var collider: CollisionPolygon2D = $Collider
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
 
 var hit_pos: Vector2 = Vector2.ZERO
 var is_on_wall: bool = false
 var area_entered: bool = false
+var ray_entered: bool = false
+var dist: Vector2
 
 
 func _ready() -> void:
 	#HACK: pin joint gets super weird at rotations around 180, so i'm rotating the child nodes instead
 	if is_left_hand:
 		$Sprite2D.rotation_degrees = 180
-		area_2d.rotation_degrees = 180
 		collider.rotation_degrees = 180
+		ray_cast_2d.position.x = - ray_cast_2d.position.x
+		ray_cast_2d.target_position = -ray_cast_2d.target_position
 		center_of_mass.x = -center_of_mass.x
 
 
 func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
-	if area_entered and is_on_wall:
-		# TODO: store collision point and normal
-		hit_pos = area_2d.get_child(0).global_position
-		# and maybe push back the axe a bit so it doesn't overlap with the wall?
+	if !ray_cast_2d.enabled:
+		return
+	
+	if ray_cast_2d.is_colliding() and !ray_entered:
+		_state.integrate_forces()
+		
+		var global_direction = ray_cast_2d.global_transform.basis_xform(ray_cast_2d.target_position).normalized()
+		print(rad_to_deg(global_direction.angle()))
+		if rad_to_deg(global_direction.angle()) > 60 or rad_to_deg(global_direction.angle()) < -60:
+			return
+		var normal = ray_cast_2d.get_collision_normal()
+		var dot: float = ray_cast_2d.get_collision_normal().dot(global_direction)
+		if dot < -0.5 or dot > 0.5:
+			ray_entered = true
+			dist = ray_cast_2d.get_collision_point() - ray_cast_2d.global_position
+			disable_motors()
+	
+	if ray_entered:
+		global_translate(dist)
+		ray_entered = false
+		ray_cast_2d.enabled = false
+		is_on_wall = true
+	
+	if is_on_wall:
+		_state.linear_velocity = Vector2.ZERO
+		_state.angular_velocity = 0
 		freeze = true
-		area_entered = false
+	
 
 
 func swing() -> void:
-	freeze = false
 	shoulder.motor_target_velocity = -shoulder_torque
 	shoulder.motor_enabled = true
 	hand.motor_target_velocity = -shoulder_torque
 	hand.motor_enabled = true
+	ray_cast_2d.enabled = true
 
 
 func drop() -> void:
 	is_on_wall = false
+	ray_cast_2d.enabled = false
 	freeze = false
 	disable_motors()
 
@@ -56,12 +82,6 @@ func disable_motors() -> void:
 	shoulder.motor_enabled = false
 	hand.motor_target_velocity = 0
 	hand.motor_enabled = false
-
-
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-	area_entered = true
-	is_on_wall = true
-	disable_motors()
 
 
 func pull() -> void:
@@ -83,9 +103,7 @@ func flip(color_flipped: bool, distance: float) -> void:
 	# and calculate the x offset between the ice axe and trigger
 	var offset = 0
 	if is_on_wall:
-		print(global_position.x)
-		print(area_2d.get_child(0).global_position.x)
-		offset = area_2d.get_child(0).global_position.x - global_position.x
+		#offset = area_2d.get_child(0).global_position.x - global_position.x
 		global_translate(Vector2(distance if color_flipped else -distance, 0))
 	
 	# Flip the sprite
@@ -112,7 +130,7 @@ func flip(color_flipped: bool, distance: float) -> void:
 	flipped_angular_limit_upper = temp_limit
 	
 	# Flip the collision mask
-	area_2d.collision_mask = LayerNames.PHYSICS_2D.WHITE if color_flipped else LayerNames.PHYSICS_2D.BLACK
+	ray_cast_2d.collision_mask = LayerNames.PHYSICS_2D.WHITE if color_flipped else LayerNames.PHYSICS_2D.BLACK
 	
 	if is_on_wall:
 		freeze = true
