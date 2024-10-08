f44c06977c5506cb3a66ddf619fefb6da62c55b6
mijkolsmith <mijkolsmith@live.com>
2024-10-08 21:00:44 +0200

    polygon tool :o

--- src/world/polygontool.gd
+++ src/world/polygontool.gd
@@ -9,36 +9,41 @@ extends CollisionPolygon2D
 			else:
 				helper = value
 
-var vis_polygon: Polygon2D
+var polygon2d: Polygon2D
 var other_collision_polygon: CollisionPolygon2D
-var other_polygon: Polygon2D
+var other_polygon2d: Polygon2D
 
 @export var generate: bool = false:
 	set(value):
 		if Engine.is_editor_hint():
 			if value:
-				vis_polygon = Polygon2D.new() as Polygon2D
-				add_child(vis_polygon)
-				vis_polygon.set_owner(get_tree().edited_scene_root)
-				vis_polygon.set_name("Polygon2D")
-				vis_polygon.polygon = polygon
-				vis_polygon.color = helper.get_color()
+				polygon2d = Polygon2D.new() as Polygon2D
+				add_child(polygon2d)
+				polygon2d.set_owner(get_tree().edited_scene_root)
+				polygon2d.set_name("Polygon2D")
+				polygon2d.polygon = polygon
+				polygon2d.color = helper.get_color()
+				polygon2d.set_script(load("res://src/world/polygon_shape_updater.gd"))
 				
 				other_collision_polygon = CollisionPolygon2D.new() as CollisionPolygon2D
-			
 				helper.other_helper.add_child(other_collision_polygon)
 				other_collision_polygon.set_owner(get_tree().edited_scene_root)
 				other_collision_polygon.set_name("OtherCollisionPolygon2D")
 				other_collision_polygon.polygon = polygon
-			
-				other_polygon = Polygon2D.new() as Polygon2D
-				other_collision_polygon.add_child(other_polygon)
-				other_polygon.set_owner(get_tree().edited_scene_root)
-				other_polygon.set_name("OtherPolygon2D")
-				other_polygon.polygon = polygon
-				other_polygon.color = helper.other_helper.get_color()
+				
+				other_polygon2d = Polygon2D.new() as Polygon2D
+				other_collision_polygon.add_child(other_polygon2d)
+				other_polygon2d.set_owner(get_tree().edited_scene_root)
+				other_polygon2d.set_name("OtherPolygon2D")
+				other_polygon2d.polygon = polygon
+				other_polygon2d.color = helper.other_helper.get_color()
+				other_polygon2d.set_script(load("res://src/world/polygon_shape_updater.gd"))
 			else:
-				vis_polygon.queue_free()
-				other_collision_polygon.queue_free()
-				other_polygon.queue_free()
+				if is_instance_valid(polygon2d):
+					polygon2d.queue_free()
+				if is_instance_valid(other_collision_polygon):
+					other_collision_polygon.queue_free()
+				if is_instance_valid(other_polygon2d):
+					other_polygon2d.queue_free()
+			
 			generate = value
