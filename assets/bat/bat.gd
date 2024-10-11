5453c19eada140aed78d212b49127a6093f3b599
Hiroshi McBride <>
2024-10-11 14:19:03 +0200

    add bat hit sound

--- assets/bat/bat.gd
+++ assets/bat/bat.gd
@@ -6,6 +6,8 @@ extends CharacterBody2D
 @onready var state_machine: StateMachine = $StateMachine
 @onready var danger_sensor_component: DangerSensorComponent = $DangerSensorComponent
 
+signal player_hit()
+
 
 func _ready() -> void:
 	health_component.die.connect(die)
@@ -27,4 +29,5 @@ func _on_hurtbox_component_body_entered(_body: Node2D) -> void:
 
 func _on_hitbox_component_area_entered(_area: Area2D) -> void:
 	# When hitting a player
+	player_hit.emit()
 	die()
