bf49a58a55bcc5d988b057ea556453e599a1d9f2
Hiroshi McBride <>
2024-10-11 14:39:18 +0200

    fix bat hit sound, add wind, reverb changes

--- assets/bat/bat.gd
+++ assets/bat/bat.gd
@@ -6,7 +6,6 @@ extends CharacterBody2D
 @onready var state_machine: StateMachine = $StateMachine
 @onready var danger_sensor_component: DangerSensorComponent = $DangerSensorComponent
 
-signal player_hit()
 
 
 func _ready() -> void:
@@ -29,5 +28,4 @@ func _on_hurtbox_component_body_entered(_body: Node2D) -> void:
 
 func _on_hitbox_component_area_entered(_area: Area2D) -> void:
 	# When hitting a player
-	player_hit.emit()
 	die()
